bf90e59e28d5a4cc422ac44dce88b8a2f67b8ce9
Weijia <xiao.wei@northeastern.edu>
2024-08-26 20:22:14 -0400

    Update to fix typo, add favicon, get rid of bug-causing unused rolling function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -169,7 +169,7 @@
 	});
 
 	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
+	    // $(document).on("scroll", onScroll);
 	    
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
@@ -188,25 +188,28 @@
 	            scrollTop: (target.offset().top) - 79
 	        }, 500, 'swing', function () {
 	            window.location.hash = target;
-	            $(document).on("scroll", onScroll);
+	            // $(document).on("scroll", onScroll);
 	        });
 	    });
 	});
 
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
+	// function onScroll(event){
+	//     var scrollPos = $(document).scrollTop();
+	//     $('.nav a').each(function () {
+	//         var currLink = $(this);
+	//         var refElement = $(currLink.attr("href"));
+	// 		if (refElement.length) {
+	// 			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	// 				$('.nav ul li a').removeClass("active");
+	// 				currLink.addClass("active");
+	// 			}
+	// 			else{
+	// 				currLink.removeClass("active");
+	// 			}
+	// 		}
+	        
+	//     });
+	// }
 
 
 	// Page loading animation
